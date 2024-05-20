9d78baf29f41594c0559e8b52d6f4f242dbe0eff
AnthonyOKC <anthonyfloresokc@gmail.com>
2024-05-20 02:52:35 -0500

    add most plugins except lsp

--- lua/anthony/lazy/harpoon.lua
+++ lua/anthony/lazy/harpoon.lua
@@ -1,6 +1,8 @@
 return {
     {
         "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
         name = "harpoon",
         config = function()
             -- REQUIRED
--- lua/packer.lua
+++ lua/packer.lua
@@ -1,18 +1,8 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-
   use {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
   }
-  use { "catppuccin/nvim", as = "catppuccin" }
 
   use {
         'nvim-treesitter/nvim-treesitter',
@@ -20,13 +10,6 @@ return require('packer').startup(function(use)
             local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
             ts_update()
         end,
-  }
-
-  use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
-  use {
-      "ThePrimeagen/harpoon",
-      branch = "harpoon2",
-      requires = { {"nvim-lua/plenary.nvim"} }
   }
 
   use 'mbbill/undotree'
@@ -43,7 +26,7 @@ return require('packer').startup(function(use)
 
   -- Powertoys like window
   use {
-  'nvim-lualine/lualine.nvim', 
+  'nvim-lualine/lualine.nvim',
   requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 }
 
@@ -65,5 +48,3 @@ return require('packer').startup(function(use)
   use 'w0rp/ale'
 
   use 'github/copilot.vim'
-
-end)

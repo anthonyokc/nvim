ac624a32bd575464f40c9ceaf743210ad9209af9
AnthonyOKC <anthonyfloresokc@gmail.com>
2024-09-14 13:58:39 -0500

    Turn off UFO - conflicts with neogit

--- lua/anthony/lazy/ufo.lua
+++ lua/anthony/lazy/ufo.lua
@@ -1,4 +1,6 @@
 return {
-    'kevinhwang91/nvim-ufo',
-    dependencies = 'kevinhwang91/promise-async',
+    -- {
+    --     'kevinhwang91/nvim-ufo',
+    --     dependencies = 'kevinhwang91/promise-async',
+    -- }
 }

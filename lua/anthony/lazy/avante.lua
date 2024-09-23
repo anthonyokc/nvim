e67a41e2f2b2537241b02353bde5fcf266e11dab
AnthonyOKC <anthonyfloresokc@gmail.com>
2024-09-22 21:19:51 -0500

    Changes to avante and copilot

--- lua/anthony/lazy/avante.lua
+++ lua/anthony/lazy/avante.lua
@@ -3,9 +3,70 @@ return {
     event = "VeryLazy",
     lazy = false,
     version = false,
+    config = function()
+        require("avante").setup({
+            provider = "claude",
+            auto_suggestions_provider = "claude",
+            hints = { enabled = false },
+            behaviour = {
+                auto_suggestions = true,
+            },
+            mappings = {
+                --- @class AvanteConflictMappings
+                diff = {
+                    ours = "co",
+                    theirs = "ct",
+                    all_theirs = "ca",
+                    both = "cb",
+                    cursor = "cc",
+                    next = "]x",
+                    prev = "[x",
+                },
+                suggestion = {
+                    accept = "<C-j>",
+                    next = "<M-]>",
+                    prev = "<M-[>",
+                    dismiss = "<C-]>",
+                },
+                jump = {
+                    next = "]]",
+                    prev = "[[",
+                },
+                submit = {
+                    normal = "<CR>",
+                    insert = "<C-s>",
+                },
+                sidebar = {
+                    switch_windows = "<Tab>",
+                    reverse_switch_windows = "<S-Tab>",
+                },
+            },
+        })
+
+        -- local Utils = require("avante.utils")
+        --
+        -- Utils.safe_keymap_set('i', '<M-]>', function()
+        --     local ctx = self:ctx()
+        --     if #ctx.suggestions == 0 then return end
+        --     ctx.current_suggestion_idx = (ctx.current_suggestion_idx % #ctx.suggestions) + 1
+        --     self:show()
+        -- end, {
+        --     desc = "avante: next suggestion",
+        --     replace_keycodes = false
+        -- })
+        --
+        -- Utils.safe_keymap_set('i', '<M-[>', function()
+        --     local ctx = self:ctx()
+        --     if #ctx.suggestions == 0 then return end
+        --     ctx.current_suggestion_idx = ((ctx.current_suggestion_idx - 2 + #ctx.suggestions) % #ctx.suggestions) + 1
+        --     self:show()
+        -- end, {
+        --     desc = "avante: previous suggestion",
+        --     replace_keycodes = false
+        -- })
+    end,
     opts = {
         -- add any opts here
-        hints = { enabled = false },
     },
     dependencies = {
         "stevearc/dressing.nvim",
@@ -13,6 +74,7 @@ return {
         "MunifTanjim/nui.nvim",
         --- The below dependencies are optional,
         "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua", -- for providers='copilot'
         {
             -- support for image pasting
             "HakonHarnes/img-clip.nvim",

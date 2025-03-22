08a378e528eeefc69679ff723e7a65087bc9e533
anthonyokc <anthonyfloresokc@gmail.com>
2025-03-22 17:36:23 -0500

    Add maximizer

---
+++ lua/anthony/lazy/maximizer.lua
@@ -0,0 +1,22 @@
+return {
+    {
+        "nvim-lua/plenary.nvim",
+        name = "plenary"
+    },
+
+    "eandrju/cellular-automaton.nvim",
+
+    {
+        "szw/vim-maximizer",
+        keys = {
+            { "<leader>k", "<cmd>MaximizerToggle<CR>", desc = "Maximize/restore current window" },
+        },
+        config = function()
+            -- Optional configuration
+            vim.g.maximizer_set_default_mapping = 0 -- Disable default mappings
+            vim.g.maximizer_restore_on_winleave = 1 -- Restore on window leave
+        end,
+    },
+
+}
+

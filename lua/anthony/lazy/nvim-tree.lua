8fd014435e39cc7ec9c9cffbf4038b11b8ae32a7
anthonyokc <anthonyfloresokc@gmail.com>
2025-03-22 17:37:24 -0500

    Toggle filters for nvim-tree

--- lua/anthony/lazy/nvim-tree.lua
+++ lua/anthony/lazy/nvim-tree.lua
@@ -23,5 +23,9 @@ return {
         -- key bindings
         vim.keymap.set("n", "<C-o>", api.tree.change_root_to_node)
         vim.keymap.set("n", "<C-g>", api.tree.toggle)
+
+        -- Toggle filters
+        vim.keymap.set("n", "<leader>th", api.tree.toggle_hidden_filter, { desc = "Toggle dotfiles" })
+        vim.keymap.set("n", "<leader>ti", api.tree.toggle_gitignore_filter, { desc = "Toggle gitignore" })
     end
 }

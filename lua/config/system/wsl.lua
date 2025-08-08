a741a62aca57988a2333c8c493c07754a3f661c3
anthonyokc <anthonyfloresokc@gmail.com>
2025-08-07 19:02:05 -0500

    commenting out the wsl clipboard

--- lua/config/system/wsl.lua
+++ lua/config/system/wsl.lua
@@ -3,12 +3,13 @@ local M = {}
 
 M.setup = function()
     -- Configure clipboard integration with Windows
-    vim.g.clipboard = {
-        name = 'wsl clipboard',
-        copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
-        paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
-        cache_enabled = true
-    }
+    -- vim.g.clipboard = {
+    --     name = 'wsl clipboard',
+    --     copy = { ["+"] = { "clip.exe" }, ["*"] = { "clip.exe" } },
+    --     paste = { ["+"] = { "nvim_paste" }, ["*"] = { "nvim_paste" } },
+    --     cache_enabled = true
+    -- }
+    vim.g.clipboard = 'osc52'
 end
 
-return M 
+return M

3c680c172b68ca457943da0a74be31be0e3899a3
anthonyokc <anthonyfloresokc@gmail.com>
2025-09-17 00:23:13 -0500

    Modify todo-comments event, so it functions again

--- lua/plugins/editor/todo.lua
+++ lua/plugins/editor/todo.lua
@@ -1,11 +1,11 @@
 return {
     "folke/todo-comments.nvim",
+    event = "BufReadPost",
     dependencies = { "nvim-lua/plenary.nvim" },
     opts = {
         -- your configuration comes here
         -- or leave it empty to use the default settings
-        -- refer to the configuration section below
+        -- refer to the configuration section here: https://github.com/folke/todo-comments.nvim?tab=readme-ov-file#%EF%B8%8F-configuration
     },
-    -- TODO: Add keymap for TodoTrouble and TodoTelescope,
     vim.keymap.set('n', '<leader>ft', "<cmd>TodoTelescope<CR>", {}),
 }

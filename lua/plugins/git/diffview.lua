c19c5c7f3ee43c6b98af96b0b57a4a9f80be38ba
anthonyokc <anthonyfloresokc@gmail.com>
2025-09-17 00:22:48 -0500

    Modify diffview event

--- lua/plugins/git/diffview.lua
+++ lua/plugins/git/diffview.lua
@@ -1,6 +1,6 @@
 return {
     "sindrets/diffview.nvim",
-    event = "VeryLazy",
+    cmd = { "DiffviewOpen", "DiffviewClose", "DiffviewToggleFiles", "DiffviewFocusFiles" },
     config = function ()
         require("diffview").setup()
         vim.keymap.set("n", "<leader>df", vim.cmd.DiffviewOpen)

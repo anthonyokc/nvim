1c708503d7b996cc3744f589fa36cfde3d5b253f
anthonyokc <anthonyfloresokc@gmail.com>
2025-09-21 20:10:11 -0500

    Add gitsigns dependecy of maximize

--- lua/plugins/git/gitsigns.lua
+++ lua/plugins/git/gitsigns.lua
@@ -1,6 +1,7 @@
 return {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
+    dependencies = { "szw/vim-maximizer" },
     config = function()
         require("gitsigns").setup {
             signs = {

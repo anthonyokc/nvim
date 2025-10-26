02c3dfd390a2d656665b2e6ab8373cef5eca36d7
Anthony Flores <anthonyfloresokc@gmail.com>
2025-10-26 18:44:07 -0500

    feat(r.nvim): Add keymap for problems(data)

--- lua/plugins/languages/r.lua
+++ lua/plugins/languages/r.lua
@@ -47,6 +47,7 @@ return {
                         -- R actions and helpers
                         { "n", "<leader><CR>",   helpers.r_action(""),            "Run (context)"  },
                         { "n", "<leader>rg",     helpers.r_action("glimpse"),     "glimpse(data)"  },
+                        { "n", "<leader>rp",     helpers.r_action("problems"),  "problems(data)" },
                         { "n", "<leader>ri", helpers.r_action(
                             '(function(package){ rlang::as_label(rlang::enexpr(package)) |> renv::install(prompt=FALSE) })'
                         ), "renv install pkg" },

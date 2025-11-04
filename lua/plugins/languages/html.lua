b232249484349cf05fd69441b3b80f64f3052c2f
Anthony Flores <anthonyfloresokc@gmail.com>
2025-11-04 08:58:38 -0600

    feat(html): Add html live preview

---
+++ lua/plugins/languages/html.lua
@@ -0,0 +1,11 @@
+return {
+    "brianhuster/live-preview.nvim",
+    ft = { "html" },
+    dependencies = {
+        -- You can choose one of the following pickers
+        "nvim-telescope/telescope.nvim",
+        "ibhagwan/fzf-lua",
+        "echasnovski/mini.pick",
+        "folke/snacks.nvim",
+    },
+}

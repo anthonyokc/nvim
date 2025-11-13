aff9841e494a72b643ececf4f32d9759f6bd39f1
Anthony Flores <anthonyfloresokc@gmail.com>
2025-11-13 00:08:38 -0600

    fix!(keymap): removed uncommonly used bufferline keymaps that were causing conflicts
    BREAKING CHANGE: Removed keybindings

--- lua/plugins/ui/tabs.lua
+++ lua/plugins/ui/tabs.lua
@@ -4,10 +4,6 @@ return {
     "akinsho/bufferline.nvim",
     event = "VeryLazy",
     keys = {
-        { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>",            desc = "Toggle Pin" },
-        { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
-        { "<leader>br", "<Cmd>BufferLineCloseRight<CR>",           desc = "Delete Buffers to the Right" },
-        { "<leader>bl", "<Cmd>BufferLineCloseLeft<CR>",            desc = "Delete Buffers to the Left" },
         { "<S-h>",      "<cmd>BufferLineCyclePrev<cr>",            desc = "Prev Buffer" },
         { "<S-l>",      "<cmd>BufferLineCycleNext<cr>",            desc = "Next Buffer" },
         { "[b",         "<cmd>BufferLineCyclePrev<cr>",            desc = "Prev Buffer" },

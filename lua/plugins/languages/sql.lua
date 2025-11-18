fdd8ab4e79762075d1a1fc508c4cac47a49f4421
Anthony Flores <anthonyfloresokc@gmail.com>
2025-11-17 22:17:04 -0600

    feat(sql): Add dadbod UI and completions

---
+++ lua/plugins/languages/sql.lua
@@ -0,0 +1,40 @@
+return {
+    -- Core: run SQL queries from Vim/Neovim
+    {
+        "tpope/vim-dadbod",
+        cmd = { "DB", "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+    },
+
+    -- UI for exploring DBs
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        cmd = { "DBUI", "DBUIToggle", "DBUIAddConnection", "DBUIFindBuffer" },
+        dependencies = {
+            "tpope/vim-dadbod",
+            { 'kristijanhusak/vim-dadbod-completion', ft = { 'sql', 'mysql', 'plsql' }, lazy = true },
+        },
+        init = function()
+            local data_path = vim.fn.stdpath("data")
+            vim.g.db_ui_auto_execute_table_helpers = 1
+            vim.g.db_ui_save_location = data_path .. "/dadbod_ui"
+            vim.g.db_ui_tmp_query_location = data_path .. "/dadbod_ui/tmp"
+            vim.g.db_ui_show_database_icon = true
+            vim.g.db_ui_use_nerd_fonts = true
+            vim.g.db_ui_use_nvim_notify = true
+            vim.g.db_ui_execute_on_save = false
+        end,
+    },
+
+    -- Completion source powered by vim-dadbod
+    {
+        "kristijanhusak/vim-dadbod-completion",
+        dependencies = { "tpope/vim-dadbod" },
+        ft = { "sql", "mysql", "plsql" },
+    },
+
+    -- Provides database access to many dbms
+    {
+        "vim-scripts/dbext.vim",
+        ft = { "sql", "mysql", "plsql" },
+    }
+}

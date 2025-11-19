14345a7835dd7b1a4e42139b150165b436a9e803
Anthony Flores <anthonyfloresokc@gmail.com>
2025-11-18 20:14:14 -0600

    chore(sql): disable dbout folding

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -78,3 +78,10 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.diagnostic.enable(false, { args.buf })
   end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "dbout",
+  callback = function()
+    vim.opt_local.foldenable = false
+  end,
+})

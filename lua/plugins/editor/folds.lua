dc627816db5aded9d39cfb25e1360a15451b9ad6
Anthony Flores <anthonyfloresokc@gmail.com>
2025-11-25 22:48:04 -0600

    feat(folds): disable ufo folding for dadbod sql filetypes

--- lua/plugins/editor/folds.lua
+++ lua/plugins/editor/folds.lua
@@ -1,12 +1,48 @@
+-- Filetypes where nvim-ufo should stay off (dadbod UI/queries)
+local dadbod_filetypes = { 'dbui', 'dbout', 'sql', 'psql' }
+local disabled_ft = {}
+for _, ft in ipairs(dadbod_filetypes) do
+    disabled_ft[ft] = true
+end
+
 return {
     {
         'kevinhwang91/nvim-ufo',
         event = 'BufReadPost',
         dependencies = 'kevinhwang91/promise-async',
+        init = function()
+            -- Autocmd to disable ufo in dadbod-related filetypes
+            local group = vim.api.nvim_create_augroup('DisableUfoForDadbodUI', { clear = true })
+            vim.api.nvim_create_autocmd('FileType', {
+                pattern = dadbod_filetypes,
+                group = group,
+                callback = function(args)
+                    -- Detach ufo to avoid conflicting visuals inside dadbod buffers
+                    local ok, ufo = pcall(require, 'ufo')
+                    if ok and disabled_ft[vim.bo[args.buf].filetype] then
+                        ufo.detach(args.buf)
+                    end
+                end,
+            })
+        end,
         config = function()
-            require('ufo').setup()
-            vim.opt.foldlevelstart = 99
+            -- Global defaults so regular buffers get modern folding by default
+            vim.opt.foldcolumn = '1'
             vim.opt.foldlevel = 99
-        end
+            vim.opt.foldlevelstart = 99
+            vim.opt.foldenable = true
+
+            local ufo = require('ufo')
+            ufo.setup({
+                provider_selector = function(_, filetype)
+                    -- Return empty selector to keep ufo off for dadbod filetypes
+                    if disabled_ft[filetype] then
+                        return ''
+                    end
+
+                    return { 'treesitter', 'indent' }
+                end,
+            })
+        end,
     }
 }

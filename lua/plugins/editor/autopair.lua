7d7ffb8970860effb2c8d08ba656798d00dd5598
Anthony Flores <anthonyfloresokc@gmail.com>
2025-12-02 21:47:44 -0600

    feat(autopair): add tab navigation out of parentheses

--- lua/plugins/editor/autopair.lua
+++ lua/plugins/editor/autopair.lua
@@ -1,7 +1,22 @@
 return {
     'windwp/nvim-autopairs',
     event = "InsertEnter",
-    config = true
-    -- use opts = {} for passing setup options
-    -- this is equalent to setup({}) function
+    opts = {
+        fast_wrap = {},
+        map_c_w = true, -- enable mapping for <C-w> to delete a pair if possible
+    },
+    config = function(_, opts)
+
+        require("nvim-autopairs").setup(opts)
+
+        -- Make <Tab> move out of parens if cursor is before ')'
+        vim.keymap.set("i", "<Tab>", function()
+            local col = vim.fn.col(".")
+            local line = vim.fn.getline(".")
+            if line:sub(col, col) == ")" then
+                return "<Right>"
+            end
+            return "<Tab>"
+        end, { expr = true })
+    end,
 }

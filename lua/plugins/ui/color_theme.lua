34cc145c171071bd856d9771eae5c75277a2d24f
Anthony Flores <anthonyfloresokc@gmail.com>
2026-09-19 17:44:49 -0500

    fix: use valid Catppuccin colorscheme name

--- lua/plugins/ui/color_theme.lua
+++ lua/plugins/ui/color_theme.lua
@@ -51,9 +51,7 @@ return {
                 },
             })
 
-            vim.cmd("colorscheme catppuccin-nvim")
-
-            ColorMyPencils()
+            ColorMyPencils("catppuccin")
         end
     },
     {
